c9b86f02fad080c177397b554a95f62a6b23bc8f
kapil-khatkar <93644719+kapil-khatkar@users.noreply.github.com>
2024-05-31 11:07:49 +0000

    string to nbr conv

---
+++ 01_basics/03.2_conversionOperation.js
@@ -0,0 +1,36 @@
+let value =3;
+console.log(" nagative value is ",-value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+//-------------String to nbr cnv------------//
+
+// console.log("1"+2);         //12
+// console.log(1+"2");         //12
+// console.log("1"+"2");       //12
+// console.log("1"+"2"+"3");   //123
+// console.log("1"+2+3);       //123
+// console.log("1"+2+"3");     //123
+// console.log(1+2+"3");       //33
+
+console.log((3*2)*5%4); 
+
+console.log(true);  //true
+console.log(+true); //1
+console.log(+"");   //0
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+
+let a = 10;
+let b = a++;
+console.log(b);
+
+let c = 20;
+let d = ++c;
+console.log(d);
