e34db77db7b0e85b073e700ef5c2e8c3cc2df570
kapil-khatkar <93644719+kapil-khatkar@users.noreply.github.com>
2024-05-31 11:43:11 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+// console.log("2">1); // true
+// console.log("02">1); //true
+
+// before comparison any two values you must check that the both valuse have same datatypes
+
+console.log(null >0);  //false
+console.log(null ==0); //false
+console.log(null >=0); //true
+console.log(null <0); //false
+
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false
+console.log(undefined >= 0); //false
+
+// ===
+console.log("2" == 2);  //true
+console.log("2" === 2); //false
