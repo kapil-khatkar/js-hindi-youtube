210955cf1a847a1b7665dd04555560750dd26495
kapil-khatkar <93644719+kapil-khatkar@users.noreply.github.com>
2024-06-03 21:25:24 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+//control flow / logic flow / conditional flow
+
+// if
+const isUserLoggedIn = true;
+if (isUserLoggedIn) {
+    console.log("Welcome");
+}
+
+const temp =35;
+if (temp<30) {
+    console.log("Cold weather");
+}else{
+    console.log("hot weather");
+}
+
+//comparison operator <, >, <=, >=, ==, !=, ===
