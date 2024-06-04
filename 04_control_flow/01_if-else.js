c93af8f6b13505dd9cc7cdfe8b5a39649f84f7e5
kapil-khatkar <93644719+kapil-khatkar@users.noreply.github.com>
2024-06-04 23:46:05 +0000

    if-else statement

--- 04_control_flow/01_if-else.js
+++ 04_control_flow/01_if-else.js
@@ -1,11 +1,13 @@
 //control flow / logic flow / conditional flow
 
-// if
+//____________if statement
 const isUserLoggedIn = true;
 if (isUserLoggedIn) {
     console.log("Welcome");
 }
 
+
+//_____________if-else
 const temp =35;
 if (temp<30) {
     console.log("Cold weather");
@@ -13,7 +15,7 @@ if (temp<30) {
     console.log("hot weather");
 }
 
-//comparison operator <, >, <=, >=, ==, !=, ===
+//__comparison operators <, >, <=, >=, ==, !=,!==, ===
 if(2 == "2"){
     console.log("executed");
 }
@@ -35,6 +37,7 @@ if(2 === "2"){
 // if (balance>500)console.log("test"),
 // console.log("test 2");// this is unreadable code
 
+//___________else-if
 const bal = 500;
 if (bal>500)console.log("test bal");
 
@@ -49,15 +52,40 @@ if(balance2<500){
     console.log("less than 1000");
 }
 
+//__LOGICAL OPERATORS => && (AND), || (OR), ! (NOT)
+
+//__________if with &&(AND) operators
 const userLoggedIn=true;
 const debitcard =true;
 const loggedInFromGoogle = false;
 const loggedInFromEmail = true;
 
-if (userLoggedIn && debitcard && 2==3) {
-    console.log("allow to buy course");
-}
-if (loggedInFromGoogle || loggedInFromEmail ) {
-    console.log("user logged in");
+if (userLoggedIn && debitcard && 2==2) {
+    console.log("user logged in (&&)");
 }
 
+//__________if with ||(OR) operators
+if (loggedInFromGoogle || loggedInFromEmail ) {
+    console.log("user logged in (||)");
+}
+
+//__________if with !(NOT) operators
+if (!loggedInFromGoogle) {
+    console.log("user logged in (!)");
+}
+
+//some extra operators
+//__Nullish Coalescing operator (??)
+let name = null;
+let text = "missing";
+let result = name ?? text;
+console.log(result); //missing
+
+//___optional chaining operator (?.)
+const car={
+    type:"fiat",
+    model:"2014",
+    color:"white"
+};
+console.log(car?.name); //undefined
+console.log(car?.color); //white
